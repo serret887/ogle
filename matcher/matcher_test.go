d22bb1eca3f113c4c8729d604eccbd390abbb8d3
Ale <serret887@gmail.com>
2017-02-02 19:40:15 -0500

    changing key for 'class' fixing failing test.

--- matcher/matcher_test.go
+++ matcher/matcher_test.go
@@ -12,13 +12,13 @@ import (
 const htmlSimple = `<div class="goma">hello</div>`
 
 var _ = Describe("Matcher/Matcher Is important to mention that thematcher are in charg of only one instance of a node they not handle a DOM they only handle one node", func() {
-	attributes := []html.Attribute{html.Attribute{Namespace: "class", Val: "goma", Key: "goma"}}
+	attributes := []html.Attribute{html.Attribute{Namespace: "class", Val: "goma", Key: "class"}}
 	nodes := &html.Node{Type: html.ElementNode, DataAtom: atom.Div, Data: "hello", Attr: attributes}
 
 	Context("Matcher WithClass", func() {
 		It("Match anything that have the class", func() {
 			m := matcher.NewWithClass("goma")
-			Expect(m.Match(nodes)).To(Equal(true), "there is no class matching in this node")
+			Expect(m.Match(nodes)).To(Equal(true), "there is no \"goma\" class matching in this node")
 		})
 
 	})
