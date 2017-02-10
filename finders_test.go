26940fb36cc9db2c7945b2512bb9a92b63bfc375
Ale <serret887@gmail.com>
2017-02-10 08:04:14 -0500

    Refactoring test.

--- finders_test.go
+++ finders_test.go
@@ -35,74 +35,78 @@ func (m *trueMatch) String() string {
 }
 
 var _ = Describe("Finders", func() {
-	o, err := ogle.New(strings.NewReader(htmlTest1))
-	if err != nil {
-		panic(err)
+	var o *ogle.Ogle
+	var err error
+	var mt matcher.Matcher
+	var mc matcher.Matcher
+	var actual []*html.Node
+
+	BeforeEach(func() {
+		var errOgle error
+		o, errOgle = ogle.New(strings.NewReader(htmlTest1))
+		if errOgle != nil {
+			panic(err)
+		}
+	})
+
+	errorFailing := func(expctedError string) {
+		Expect(actual).To(BeNil())
+		Expect(expctedError).To(BeEquivalentTo(err.Error()))
+
 	}
+
 	Context("First method", func() {
 		It("Return the first element with  the <title> tag", func() {
-			m := matcher.WithTag(atom.Title)
-			actual, err := o.First(m)
+			mt = matcher.WithTag(atom.Title)
+			actual, err := o.First(mt)
 			Expect(err).To(BeNil())
 			Expect(actual).ToNot(BeNil(), "should only return one element")
 			Expect(actual.FirstChild.Data).To(BeEquivalentTo(" test HTML "))
 		})
 	})
 
-	// Context("Last Method", func() {
-	// 	It("Return the last element that implement the matcher", func() {
-	// 		m := matcher.NewByTag(atom.Div)
-	// 		actual, err := o.Last(m)
-	// 		Expect(err).To(BeNil())
-	// 		Expect(actual).ToNot(BeNil(), "should only return one element")
-
-	// 	})
-	// })
-
 	Context("Custom Matchers", func() {
 
 		It(" True to any node, return all the nodes", func() {
-			mc := &trueMatch{}
-			actual, err := o.Find(mc)
+			m := &trueMatch{}
+			actual, err := o.Find(m)
 			Expect(err).To(BeNil())
 			Expect(len(actual)).To(Equal(22), "The amount of nodes in the html")
 
 		})
 		It("False to any node, return a proper error", func() {
-			mc := &falseMatch{}
-			actual, err := o.Find(mc)
-			Expect(actual).To(BeNil())
-			Expect("Problems Finding tag").To(BeEquivalentTo(err.Error()))
+			m := &falseMatch{}
+			actual, err = o.Find(m)
+			errorFailing("Problems Finding tag")
 		})
 	})
 
 	Context("Find all the nodes with an specific MATCHER", func() {
 
 		It("Return an error if there is no match", func() {
-			m := matcher.WithTag(atom.Action)
-			actual, err := o.Find(m)
-			Expect(actual).To(BeNil())
-			Expect("Problems Finding tag " + atom.Action.String()).To(BeEquivalentTo(err.Error()))
+			mt = matcher.WithTag(atom.Action)
+			actual, err = o.Find(mt)
+			errorFailing("Problems Finding tag " + atom.Action.String())
 		})
 		It("Return all the nodes with the <div> tag", func() {
-			m := matcher.WithTag(atom.Div)
-			actual, err := o.Find(m)
+			mt = matcher.WithTag(atom.Div)
+			actual, err := o.Find(mt)
 			Expect(err).To(BeNil())
 			Expect(len(actual)).To(Equal(3), "the amount of div should be 3")
 		})
 		It("Finding when there is only one match", func() {
-			m := matcher.WithTag(atom.Title)
-			actual, err := o.First(m)
+			mc = matcher.WithTag(atom.Title)
+			actual, err := o.Find(mc)
 			Expect(err).To(BeNil())
 			Expect(actual).ToNot(BeNil(), "should only return one element")
-			Expect(actual.FirstChild.Data).To(BeEquivalentTo(" test HTML "))
+			Expect(actual[0].FirstChild.Data).To(BeEquivalentTo(" test HTML "))
 
 		})
 
 		It("Mixing different matches together in a single search", func() {
-			mt := matcher.WithTag(atom.Div)
-			m := matcher.WithClass("container")
-			actual, err := o.Find(m, mt)
+			mt = matcher.WithTag(atom.Div)
+			mc = matcher.WithClass("container")
+			actual, err := o.Find(mc, mt)
 			Expect(err).To(BeNil())
 			Expect(len(actual)).To(Equal(1), "Should only find one node")
 			Expect(actual[0].DataAtom).To(Equal(atom.Div), "should be a div tag")
